3f0b15198a28f81832c2df4e3c12652435eaeb0c
TrofimenkoYurii <tyaa@ukr.net>
2018-06-10 19:04:55 +0300

    Добавьте файлы проекта.

---
+++ WebBrowserWpf/HtmlBridge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace WebBrowserWpf
+{
+    [ComVisibleAttribute(true)]
+    public class HtmlBridge
+    {
+        [ComVisibleAttribute(true)]
+        public void TestCs(int a, int b)
+        {
+            int c = TestCsBase(a, b);
+            //MessageBox.Show("Result: " + a + " + " + b + " = " + c.ToString());
+            MessageBox.Show("Result: " + a + " + " + b + " = " + c);
+        }
+
+        [ComVisibleAttribute(true)]
+        public string TestCs2(int a, int b)
+        {
+            int c = TestCsBase(a, b);
+            return "Result: " + a + " + " + b + " = " + c;
+        }
+
+        private int TestCsBase(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ WebBrowserWpf/Window1.xaml.cs
@@ -0,0 +1,87 @@
+﻿using mshtml;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WebBrowserWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private HtmlBridge htmlBridge;
+        //MainWindow objMainWindow;
+        public Window1()
+        {
+            InitializeComponent();
+
+            htmlBridge = new HtmlBridge();
+        }
+
+        /*private void ProcessElement(IHTMLElement iHTMLElement, ItemCollection itemCollection)
+        {
+            throw new NotImplementedException();
+        }*/
+
+        private void ProcessElement(IHTMLElement parentElement, ItemCollection nodes)
+        {
+            //set the Microsoft HTML Object Library
+            foreach (IHTMLElement element in (IHTMLElementCollection)parentElement.children)
+            {
+                TreeViewItem node = new TreeViewItem();
+                node.Header = "<" + element.tagName + ">";
+                nodes.Add(node);
+                if ((((IHTMLElementCollection)(element.children)).length == 0) && (element.innerText != null))
+                {
+                    node.Items.Add(element.innerText);
+                }
+                else
+                {
+                    ProcessElement(element, node.Items);
+                }
+            }
+        }
+
+        private void treeView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+
+            // Объект DOM из WebBrowser
+            HTMLDocument dom = (HTMLDocument)webBrowser.Document;
+            ProcessElement(dom.documentElement, treeView.Items);
+
+            this.Cursor = null;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            goHome();
+        }
+
+        private void homeButton_Click(object sender, RoutedEventArgs e)
+        {
+            goHome();
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            //webBrowser.ObjectForScripting = htmlBridge;
+            webBrowser.Navigate(
+                new Uri("file:///"
+                    + Path.Combine(Path.GetDirectoryName(Application.ResourceAssembly.Location)
+                        , "Html/about.html")));
+        }
+
+        private void goHome() {
+
+            webBrowser.ObjectForScripting = htmlBridge;
+            webBrowser.Navigate(
+                new Uri("file:///"
+                    + Path.Combine(Path.GetDirectoryName(Application.ResourceAssembly.Location)
+                        , "Html/index.html")));
+        }
+    }
+}
